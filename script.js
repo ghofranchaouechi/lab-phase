3b52839675a3cd59c1c7e50220d26308a38f83fb
ghofranchaouech <chaouechig@gmail.com>
2024-06-13 19:51:51 +0100

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
