94dd6eac84c439d54ea988d6aeddc9a60a25838d
ghofranchaouech <chaouechig@gmail.com>
2024-07-10 12:35:41 +0100

    last-update

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ menu.onclick = () => {
   menu.classList.toggle("move");
   navbar.classList.toggle("open-menu");
 };
+// close menu on scroll
+window.onscroll = () => {
+  menu.classList.remove("move");
+  navbar.classList.remove("open-menu");
+};
+// // scrollreveal animation
+// const animate = scrollReveal({
+//   origin: "top",
+//   distance: "60px",
+//   duration: "2500",
+//   delay: "400",
+// });
+// animate.reveal(".nav");
+// animate.reveal(".home-text", { origin: "left" });
+// animate.reveal(".home-img", { origin: "bottom" });
+// animate.reveal(".ser-box, .product-box, .team-box, .book-data", {
+//   interval: 100,
+// });
